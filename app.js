f2c8c84108cfd91d9b7fb4bcb2ce70f18052dc16
santhoshmp89 <santhoshmp89@gmail.com>
2015-09-07 15:33:34 +0530

    React App
    React App

---
+++ app.js
@@ -0,0 +1,9 @@
+var connect = require('connect');
+
+var app = connect()
+			.use(connect.static('public'))
+			.use(function(req, res){
+					res.end("Couldn't find it");
+			})
+			.listen(3000);
+			console.log("localhost:3000");
---
+++ public/js/Demo.jsx
@@ -0,0 +1,12 @@
+var SlideShow = React.createClass({
+  render: function(){
+    return (
+      <div className="banner-block">
+        <img src="./images/banner.png" />
+        <h1 className="heading">Lorem Ipsum is simply dummy text</h1>
+		<p className="description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, </p>
+      </div>
+    );
+  }
+});
+
